0bc857859be1014486de02c58009e04c595a61fe
preetimenghwani <priti@iitk.ac.in>
2020-08-19 03:09:26 +0530

    added comments

--- peripherals/soc_main/top.vhd
+++ peripherals/soc_main/top.vhd
@@ -1368,7 +1368,8 @@ begin
 	    OREG_SIZE => OREG_SIZE,
 	    IREG_SIZE => IREG_SIZE,
 	    INITIAL =>
-		(11 => x"00000131", others => (others => '0')) )
+		(11 => x"00000131",        --all stopped/reset
+		others => (others => '0')) )
 	port map (
 	    s_axi_aclk => m_axi0a_aclk(1),
 	    s_axi_areset_n => m_axi0a_areset_n(1),
@@ -2502,7 +2503,8 @@ begin
 	    OREG_SIZE => OGEN_SIZE,
 	    IREG_SIZE => IGEN_SIZE,
 	    INITIAL =>
-		(11 => x"00000321", others => (others => '0')) )
+		(11 => x"00000321",        -- all stopped/reset 
+		others => (others => '0')) )
 	port map (
 	    s_axi_aclk => m_axi1a_aclk(1),
 	    s_axi_areset_n => m_axi1a_areset_n(1),
